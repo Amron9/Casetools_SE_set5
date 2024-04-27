26bd9d216b9643a478390d28f72c511d03a34912
Amron9 <Amroncosta123@gmail.com>
2024-04-27 09:51:54 +0530

    inital commit

---
+++ src/House109.java
@@ -0,0 +1,12 @@
+public class House109 {
+    String color;
+    String Owner;
+    House109 (String color, String owner) {
+        this.color = color;
+        this.Owner = owner;
+    }
+    String getOwner()
+    {
+        return Owner;
+    }
+}
